e05c869d582b0efcb3130d06aa44b002fe0636aa
Maxim Tynyanko <m.tynyanko@tg.dunice.net>
2024-06-26 09:49:53 +0300

    new functions

--- main.js
+++ main.js
@@ -1,29 +1,55 @@
 let input = document.getElementById("inputId");
 const button = document.getElementById("buttonId");
 const container = document.querySelector(".container");
-const checkbox = document.getElementById("checkId");
+//const checkbox = document.getElementById("checkId");
 
-const tasks=[];
+/*const tasks=[];
+tasks[0] = {text:0,
+    check:0,
+    id:0
+}
+*/
 let n = 0;
+
+function layout() {
+    
+}
 
 function createNewTask() {
     n += 1;
     let newTask = {
         text:input.value,
         check:false,
-        id:n
+        id:Date.now()
     };
-    tasks.push(newTask)
-    newTaskHTML = '<p><input type="checkbox" id="checkId" name="check" />'+
-    '<label for="check">'+' '+newTask.text+'</label></p>';
-    container.innerHTML += newTaskHTML;
+    tasks.push(newTask);
     console.log(tasks);
 };
+
+
+
+function renderAllTasks() {
+    for (let i = 1; i <= n; i++) {
+        let currentTask = tasks[i];
+        newTaskHTML = '<p><input type="checkbox" id="'+ currentTask.id +'" name="check" />'+
+        '<label for="check">'+' '+currentTask.text+'</label></p>';
+        container.innerHTML += newTaskHTML;
+    }
+}
+
+function renderLastTask() {
+    let currentTask = tasks[n];
+    newTaskHTML = '<p><input type="checkbox" id="'+ currentTask.id +'" name="check" />'+
+    '<label for="check">'+' '+currentTask.text+'</label></p>';
+    container.innerHTML += newTaskHTML;
+}
+
+
 
 function checkCheckBox() {
     console.log('click!')
 }
 
-
 button.addEventListener('click', createNewTask);
-checkbox.addEventListener('click', checkCheckBox);
+button.addEventListener('click', renderLastTask);
+//heckbox.addEventListener('click', checkCheckBox);
